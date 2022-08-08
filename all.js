3a2926063a38592c1b2101f16a515a1d7025762c
Michael0520 <po2319987@gmail.com>
2022-08-08 23:30:34 +0800

    fix

--- all.js
+++ all.js
@@ -4,33 +4,28 @@ function App() {
     {
       name: "日幣",
       rate: 0.2128,
-      newMoney: 0,
     },
     {
       name: "美金",
       rate: 29.555,
-      newMoney: 0,
     },
     {
       name: "澳幣",
       rate: 20.31,
-      newMoney: 0,
     },
     {
       name: "韓幣",
       rate: 0.02124,
-      newMoney: 0,
     },
     {
       name: "印尼幣",
       rate: 0.00168,
-      newMoney: 0,
     },
   ];
   // 處理 money 的 state
   const [money, setMoney] = React.useState(0);
   // 處理計算後的 money，初始值是物件的陣列 rates
-  const [results, setResults] = React.useState(rates);
+  const [newMoney, setNewMoney] = React.useState(0);
 
   // 讀取並且更新每一次的 money 變數
   const handleMoneyChange = (e) => {
@@ -38,24 +33,15 @@ function App() {
   };
 
   // 按下計算按鈕後觸發事件
-  // 用 map 把目前的 results 陣列解開並回傳計算後的 money
-  const handleCalculate = (e) => {
-    e.preventDefault();
-    setResults(
-      results.map((result) => {
-        return {
-          name: result.name,
-          rate: result.rate,
-          newMoney: (money / result.rate).toFixed(2),
-        };
-      })
-    );
+  // 讀取 money 當前的數值，使用 setNewMoney 動態地將此數值賦予到 newMoney 上
+  const handleCalculate = () => {
+    setNewMoney(money);
   };
 
   return (
     <div className="bg-slate-500 min-h-screen text-white text-lg">
       <div className="flex flex-col p-10 mx-auto max-w-xl">
-        <form>
+        <form onSubmit={(e) => e.preventDefault()}>
           <label className="block text-2xl">
             <span className="block text-2xl font-medium">
               請輸入您要換的台幣
@@ -67,19 +53,19 @@ function App() {
               placeholder="台幣"
               onChange={handleMoneyChange}
             />
-            <button
+            <input
+              type="button"
+              value="開始計算"
               className="rounded bg-blue-400 text-white px-5 py-2 ml-4"
               onClick={handleCalculate}
-            >
-              開始計算
-            </button>
+            />
           </label>
         </form>
         <ul className="list-disc mt-5 text-2xl">
           可以換算成以下類別:
-          {results.map((result, index) => (
+          {rates.map((result, index) => (
             <li key={index} className="ml-5 mt-2">
-              {result.name}：{result.newMoney ? result.newMoney : null}
+              {result.name}：{(newMoney * result.rate).toFixed(2)}
             </li>
           ))}
         </ul>
